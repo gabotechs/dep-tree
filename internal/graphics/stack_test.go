c950380030560f0479005933db6f850fff96d19f
Gabriel Musat <gabimtme@hotmail.com>
2022-12-12 16:15:11 +0100

    chore: add stack tests

--- internal/graphics/stack_test.go
+++ internal/graphics/stack_test.go
@@ -17,3 +17,37 @@ func TestCellStack_Render_lines(t *testing.T) {
 	}))
 	a.Equal("╷", cs.Render())
 }
+
+func TestCellStack_Render_charHasPriority(t *testing.T) {
+	a := require.New(t)
+	cs := CellStack{}
+	cs.add(LinesCell(Lines{
+		t: true,
+	}))
+	cs.PlaceChar('a')
+	a.Equal("a", cs.Render())
+}
+
+func TestCellStack_Render_arrowHasPriority(t *testing.T) {
+	a := require.New(t)
+	cs := CellStack{}
+	cs.PlaceArrow(false)
+	cs.add(LinesCell(Lines{
+		t: true,
+	}))
+	a.Equal("▷", cs.Render())
+}
+
+func TestCellStack_Tag(t *testing.T) {
+	a := require.New(t)
+	cs := CellStack{}
+
+	a.Equal(false, cs.Is("key", "foo"))
+	cs.Tag("key", "bar")
+	a.Equal(false, cs.Is("key", "foo"))
+	a.Equal(true, cs.Is("key", "bar"))
+	cs.Tag("key", "foo")
+	cs.Tag("otherKey", "bar")
+	a.Equal(true, cs.Is("key", "foo"))
+	a.Equal(true, cs.Is("otherKey", "bar"))
+}
