7f0cef8172cd6d1a1dc9b0fdda16dad3711f78c9
Gabriel Musat <gabimtme@hotmail.com>
2022-12-24 17:57:18 +0100

    chore: add more resolve tests

--- internal/js/resolve_test.go
+++ internal/js/resolve_test.go
@@ -5,20 +5,41 @@ import (
 	"path"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 const resolverTestFolder = ".resolve_test"
 
+func TestParser_ResolvePath_IsCached(t *testing.T) {
+	a := require.New(t)
+	ctx := context.Background()
+	parser, err := MakeJsParser(resolverTestFolder)
+	a.NoError(err)
+
+	start := time.Now()
+	ctx, _, err = parser.ResolvePath(ctx, path.Join(resolverTestFolder, "src", "foo.ts"), resolverTestFolder)
+	a.NoError(err)
+	nonCached := time.Since(start)
+
+	start = time.Now()
+	_, _, err = parser.ResolvePath(ctx, path.Join(resolverTestFolder, "src", "foo.ts"), resolverTestFolder)
+	a.NoError(err)
+	cached := time.Since(start)
+
+	a.Greater(nonCached.Nanoseconds(), cached.Nanoseconds())
+}
+
 func TestParser_ResolvePath(t *testing.T) {
 	absPath, _ := filepath.Abs(resolverTestFolder)
 
 	tests := []struct {
-		Name       string
-		Unresolved string
-		Cwd        string
-		Resolved   string
+		Name          string
+		Unresolved    string
+		Cwd           string
+		Resolved      string
+		ExpectedError string
 	}{
 		{
 			Name:       "from relative",
@@ -44,6 +65,30 @@ func TestParser_ResolvePath(t *testing.T) {
 			Unresolved: "@/helpers/diff",
 			Resolved:   path.Join(absPath, "src", "helpers", "diff.ts"),
 		},
+		{
+			Name:          "Does not resolve invalid import",
+			Cwd:           path.Join(resolverTestFolder, "src"),
+			Unresolved:    "bar",
+			ExpectedError: "import 'bar' cannot be resolved",
+		},
+		{
+			Name:          "Does not resolve invalid relative import",
+			Cwd:           path.Join(resolverTestFolder, "src", "utils"),
+			Unresolved:    "./foo",
+			ExpectedError: "could not perform relative import for './foo' because the file or dir was not found",
+		},
+		{
+			Name:          "Does not resolve invalid relative import",
+			Cwd:           resolverTestFolder,
+			Unresolved:    path.Join("src", "utils", "foo"),
+			ExpectedError: "import 'src/utils/foo' cannot be resolved",
+		},
+		{
+			Name:          "Does not resolve invalid path override import",
+			Cwd:           path.Join(resolverTestFolder, "src"),
+			Unresolved:    "@/helpers/bar",
+			ExpectedError: "import '@/helpers/bar' was matched to path '@/helpers/' in tscofing's paths option, but the resolved path did not match an existing file",
+		},
 	}
 
 	for _, tt := range tests {
@@ -52,8 +97,12 @@ func TestParser_ResolvePath(t *testing.T) {
 			parser, err := MakeJsParser(tt.Cwd)
 			a.NoError(err)
 			_, resolved, err := parser.ResolvePath(context.Background(), tt.Unresolved, tt.Cwd)
-			a.NoError(err)
-			a.Equal(tt.Resolved, resolved)
+			if tt.ExpectedError != "" {
+				a.ErrorContains(err, tt.ExpectedError)
+			} else {
+				a.NoError(err)
+				a.Equal(tt.Resolved, resolved)
+			}
 		})
 	}
 }
