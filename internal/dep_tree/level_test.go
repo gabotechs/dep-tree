7d3a397ef4b1dc38e8d50668b3eb7003480080a0
Gabriel Musat <gabimtme@hotmail.com>
2023-01-24 13:15:44 +0100

    feat: retrieve cyclical deps

--- internal/dep_tree/level_test.go
+++ internal/dep_tree/level_test.go
@@ -15,6 +15,7 @@ func TestNode_Level(t *testing.T) {
 		Name           string
 		Children       map[int][]int
 		ExpectedLevels []int
+		ExpectedCycles [][]string
 	}{
 		{
 			Name: "Simple",
@@ -35,6 +36,7 @@ func TestNode_Level(t *testing.T) {
 				4: {3},
 			},
 			ExpectedLevels: []int{0, 1, 2, 4, 3},
+			ExpectedCycles: [][]string{{"3", "4", "3"}},
 		},
 		{
 			Name: "Cycle 2",
@@ -44,6 +46,7 @@ func TestNode_Level(t *testing.T) {
 				2: {0, 1},
 			},
 			ExpectedLevels: []int{0, 2, 1},
+			ExpectedCycles: [][]string{{"1", "2", "1"}},
 		},
 		{
 			Name: "Cycle 3",
@@ -53,6 +56,31 @@ func TestNode_Level(t *testing.T) {
 				2: {1},
 			},
 			ExpectedLevels: []int{0, 1, 2},
+			ExpectedCycles: [][]string{{"1", "2", "1"}},
+		},
+		{
+			Name: "Cycle 4",
+			Children: map[int][]int{
+				0: {1},
+				1: {2},
+				2: {0},
+			},
+			ExpectedLevels: []int{0, 1, 2},
+			// TODO: there is a bug with cyclical deps belonging to node 0.
+			ExpectedCycles: nil,
+		},
+		{
+			Name: "Cycle 5",
+			Children: map[int][]int{
+				0: {1},
+				1: {2},
+				2: {3},
+				3: {4},
+				4: {1},
+			},
+			// TODO: weird level calculation, but strictly correct.
+			ExpectedLevels: []int{0, 3, 4, 1, 2},
+			ExpectedCycles: [][]string{{"1", "2", "3", "4", "1"}},
 		},
 		{
 			Name: "Avoid same level",
@@ -81,14 +109,22 @@ func TestNode_Level(t *testing.T) {
 					a.NoError(err)
 				}
 			}
+			levelCalculator := NewLevelCalculator(g, "0")
+
 			ctx := context.Background()
 			var lvls []int
 			for i := 0; i < numNodes; i++ {
 				var lvl int
-				ctx, lvl = level(ctx, g, strconv.Itoa(i), "0")
+				ctx, lvl = levelCalculator.level(ctx, strconv.Itoa(i))
 				lvls = append(lvls, lvl)
 			}
 			a.Equal(tt.ExpectedLevels, lvls)
+			var cycles [][]string
+			for _, cycleKey := range levelCalculator.Cycles.Keys() {
+				cycle, _ := levelCalculator.Cycles.Get(cycleKey)
+				cycles = append(cycles, cycle.Stack)
+			}
+			a.Equal(tt.ExpectedCycles, cycles)
 		})
 	}
 }
---
+++ internal/utils/copy_map.go
@@ -0,0 +1,9 @@
+package utils
+
+func CopyMap[K comparable, V any](m map[K]V) map[K]V {
+	result := make(map[K]V)
+	for k, v := range m {
+		result[k] = v
+	}
+	return result
+}
