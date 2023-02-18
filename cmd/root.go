627e1e890b30ebffb1909a2f085f58f341e38dcf
Gabriel Musat <gabimtme@hotmail.com>
2023-02-18 17:49:35 +0100

    chore: set silence usage

--- cmd/root.go
+++ cmd/root.go
@@ -16,11 +16,12 @@ func endsWith(string string, substrings []string) bool {
 }
 
 var Root = &cobra.Command{
-	Use:   "dep-tree",
-	Short: "Visualize and check your project's dependency tree",
+	Use:          "dep-tree",
+	Short:        "Visualize and check your project's dependency tree",
+	SilenceUsage: true,
 	Long: `
       ____         _ __       _
-     |  _ \   ___ |  _ \    _| |_  _ __  ___   ___ 
+     |  _ \   ___ |  _ \    _| |_  _ __  ___   ___
      | | | | / _ \| |_) |  |_   _||  __|/ _ \ / _ \
      | |_| ||  __/| .__/     | |  | |  |  __/|  __/
      |____/  \__| |_|        | \__|_|   \___| \___|
