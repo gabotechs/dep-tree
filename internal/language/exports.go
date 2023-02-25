5bffacf17c18b4eb30d55a35501cffae9b5ed530
Gabriel Musat <gabimtme@hotmail.com>
2023-02-25 10:18:57 +0100

    style: lint

--- internal/language/exports.go
+++ internal/language/exports.go
@@ -4,6 +4,6 @@ type ExportsResult struct {
 	// Exports: map from exported name to the absolute path from where it is exported
 	//  NOTE: even though it could work returning a path relative to the file, it should return absolute
 	Exports map[string]string
-	// Errors: errors while parsing exports
+	// Errors: errors while parsing exports.
 	Errors []error
 }
--- internal/language/imports.go
+++ internal/language/imports.go
@@ -6,6 +6,6 @@ type ImportsResult struct {
 	// Imports: ordered map from absolute imported path to the array of names that where imported.
 	//  if one of the names is *, then all the names are imported
 	Imports *orderedmap.OrderedMap[string, []string]
-	// Errors: errors while parsing imports
+	// Errors: errors while parsing imports.
 	Errors []error
 }
