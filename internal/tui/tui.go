9037948a4edc2693287ed2d7ed2eff171125f38a
Gabriel Musat <gabimtme@hotmail.com>
2023-03-02 19:45:04 +0100

    refactor: better tui testing system

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -13,11 +13,31 @@ import (
 
 var style = tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
 
+type LoopTestOverrides struct {
+	Screen tcell.SimulationScreen
+
+	ManualPump bool
+	Pump       func(event tcell.Event) error
+}
+
+func initScreen() (tcell.Screen, error) {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		return nil, err
+	}
+	err = screen.Init()
+	if err != nil {
+		return nil, err
+	}
+	screen.SetStyle(style)
+	return screen, nil
+}
+
 func Loop[T any](
 	ctx context.Context,
 	initial string,
 	parserBuilder func(string) (dep_tree.NodeParser[T], error),
-	screen tcell.Screen,
+	testOverrides *LoopTestOverrides,
 ) error {
 	parser, err := parserBuilder(initial)
 	if err != nil {
@@ -35,18 +55,13 @@ func Loop[T any](
 	if err != nil {
 		return err
 	}
-	if screen == nil {
-		if screen, err = tcell.NewScreen(); err != nil {
+	var screen tcell.Screen
+	if testOverrides == nil || testOverrides.Screen == nil {
+		if screen, err = initScreen(); err != nil {
 			return err
 		}
-	}
-	err = screen.Init()
-	if err != nil {
-		return err
-	}
-	screen.SetStyle(style)
-	if err != nil {
-		return err
+	} else {
+		screen = testOverrides.Screen
 	}
 
 	renderState := &systems.RenderState{
@@ -67,7 +82,7 @@ func Loop[T any](
 		SelectedId: "",
 		Event:      nil,
 		OnNavigate: func(s *systems.State) error {
-			return Loop[T](ctx, s.SelectedId, parserBuilder, nil)
+			return Loop[T](ctx, s.SelectedId, parserBuilder, testOverrides)
 		},
 	}
 
@@ -91,6 +106,27 @@ func Loop[T any](
 			return err
 		}
 		screen.Show()
-		globalState.Event = screen.PollEvent()
+		if testOverrides != nil && testOverrides.ManualPump {
+			testOverrides.Pump = makeTestPump(globalState, world)
+			return nil
+		} else {
+			globalState.Event = screen.PollEvent()
+		}
+	}
+}
+
+func makeTestPump(s *systems.State, world *ecs.World) func(event tcell.Event) error {
+	return func(event tcell.Event) error {
+		s.Event = event
+		s.Screen.Clear()
+		err := world.Update()
+		switch {
+		case systems.IsShouldQuit(err):
+			return nil
+		case err != nil:
+			return err
+		}
+		s.Screen.Show()
+		return nil
 	}
 }
