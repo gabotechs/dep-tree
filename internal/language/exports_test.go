f20c9024a7f51e85f8d06b26c3da4e428c8f9099
Gabriel Musat <gabimtme@hotmail.com>
2023-03-03 20:51:37 +0100

    chore: better exports testing helpers

--- internal/language/exports_test.go
+++ internal/language/exports_test.go
@@ -2,6 +2,7 @@ package language
 
 import (
 	"context"
+	"strings"
 	"testing"
 	"time"
 
@@ -9,18 +10,53 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type ExportsResultBuilder map[string]*ExportsResult
+
+func (e *ExportsResultBuilder) Build() map[string]*ExportsResult {
+	return *e
+}
+
+func (e *ExportsResultBuilder) Entry(inId string, toId string, names ...string) *ExportsResultBuilder {
+	var result *ExportsResult
+	var ok bool
+	if result, ok = (*e)[inId]; !ok {
+		result = &ExportsResult{}
+	}
+
+	if len(names) == 1 && names[0] == "*" {
+		result.Exports = append(result.Exports, ExportEntry{
+			All: true,
+			Id:  toId,
+		})
+	} else {
+		var n []ExportName
+		for _, name := range names {
+			if strings.HasPrefix(name, "as ") {
+				n[len(n)-1].Alias = strings.TrimLeft(name, "as ")
+			} else {
+				n = append(n, ExportName{Original: name})
+			}
+		}
+		result.Exports = append(result.Exports, ExportEntry{
+			Names: n,
+			Id:    toId,
+		})
+	}
+	(*e)[inId] = result
+	return e
+}
+
+func b() *ExportsResultBuilder {
+	return &ExportsResultBuilder{}
+}
+
 func TestParser_parseExports_IsCached(t *testing.T) {
 	a := require.New(t)
 	ctx := context.Background()
 	lang := TestLanguage{
-		exports: map[string]*ExportsResult{
-			"1": {
-				Exports: []ExportEntry{{
-					Names: []ExportName{{Original: "A"}},
-					Id:    "1",
-				}},
-			},
-		},
+		exports: b().
+			Entry("1", "1", "A").
+			Build(),
 	}
 
 	parser, err := makeParser("1", func(_ string) (Language[TestLanguageData, TestFile], error) {
@@ -60,14 +96,9 @@ func TestParser_CachedUnwrappedParseExports(t *testing.T) {
 		{
 			Name: "direct export",
 			Id:   "1",
-			Exports: map[string]*ExportsResult{
-				"1": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "A"}},
-						Id:    "1",
-					},
-				}},
-			},
+			Exports: b().
+				Entry("1", "1", "A").
+				Build(),
 			Expected: makeStringOm(
 				"A", "1",
 			),
@@ -75,20 +106,10 @@ func TestParser_CachedUnwrappedParseExports(t *testing.T) {
 		{
 			Name: "one proxy",
 			Id:   "1",
-			Exports: map[string]*ExportsResult{
-				"1": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "A"}},
-						Id:    "2",
-					},
-				}},
-				"2": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "A"}},
-						Id:    "2",
-					},
-				}},
-			},
+			Exports: b().
+				Entry("1", "2", "A").
+				Entry("2", "2", "A").
+				Build(),
 			Expected: makeStringOm(
 				"A", "2",
 			),
@@ -96,26 +117,11 @@ func TestParser_CachedUnwrappedParseExports(t *testing.T) {
 		{
 			Name: "double proxy",
 			Id:   "1",
-			Exports: map[string]*ExportsResult{
-				"1": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "A"}},
-						Id:    "2",
-					},
-				}},
-				"2": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "A"}},
-						Id:    "3",
-					},
-				}},
-				"3": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "A"}},
-						Id:    "3",
-					},
-				}},
-			},
+			Exports: b().
+				Entry("1", "2", "A").
+				Entry("2", "3", "A").
+				Entry("3", "3", "A").
+				Build(),
 			Expected: makeStringOm(
 				"A", "3",
 			),
@@ -123,26 +129,11 @@ func TestParser_CachedUnwrappedParseExports(t *testing.T) {
 		{
 			Name: "double proxy with alias",
 			Id:   "1",
-			Exports: map[string]*ExportsResult{
-				"1": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "A"}},
-						Id:    "2",
-					},
-				}},
-				"2": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "B", Alias: "A"}},
-						Id:    "3",
-					},
-				}},
-				"3": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "C", Alias: "B"}},
-						Id:    "3",
-					},
-				}},
-			},
+			Exports: b().
+				Entry("1", "2", "A").
+				Entry("2", "3", "B", "as A").
+				Entry("3", "3", "C", "as B").
+				Build(),
 			Expected: makeStringOm(
 				"A", "3",
 			),
@@ -150,26 +141,11 @@ func TestParser_CachedUnwrappedParseExports(t *testing.T) {
 		{
 			Name: "double proxy with all export",
 			Id:   "1",
-			Exports: map[string]*ExportsResult{
-				"1": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "A"}},
-						Id:    "2",
-					},
-				}},
-				"2": {Exports: []ExportEntry{
-					{
-						All: true,
-						Id:  "3",
-					},
-				}},
-				"3": {Exports: []ExportEntry{
-					{
-						Names: []ExportName{{Original: "C", Alias: "A"}},
-						Id:    "3",
-					},
-				}},
-			},
+			Exports: b().
+				Entry("1", "2", "A").
+				Entry("2", "3", "*").
+				Entry("3", "3", "C", "as A").
+				Build(),
 			Expected: makeStringOm(
 				"A", "3",
 			),
