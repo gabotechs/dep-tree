14cf6624c59d5438a0f198b71be4f3586987f930
Gabriel Musat <gb.mt.me@gmail.com>
2023-08-14 18:17:13 +0200

    chore: add test

--- internal/python/.imports_test/main.py
+++ internal/python/.imports_test/main.py
@@ -5,8 +5,10 @@ import sys
 from sys import something
 from src.main import main
 import asyncio
-from .src import main
-from .src.main import main
+try:
+    from .src import main
+except Exception as e:
+    from .src.main import main
 from . import src
 from . import un_existing
 from src.module import *
