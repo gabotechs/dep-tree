995b738b5290a07cba7493cf177dcde0f9526811
Gabriel Musat <gb.mt.me@gmail.com>
2023-12-23 13:06:58 +0100

    add gui to entropy rendering

--- internal/entropy/graph.go
+++ internal/entropy/graph.go
@@ -1,7 +1,6 @@
 package entropy
 
 import (
-	"encoding/json"
 	"path"
 
 	"github.com/gabotechs/dep-tree/internal/dep_tree"
@@ -10,10 +9,7 @@ import (
 )
 
 const (
-	maxNodeSize         = 10
-	folderNodeRepulsion = 1
-	folderLinkStrength  = 2
-	cyclicDepColor      = "indianred"
+	maxNodeSize = 10
 )
 
 type Node struct {
@@ -23,29 +19,23 @@ type Node struct {
 	Loc      int    `json:"loc"`
 	Size     int    `json:"size"`
 	Color    []int  `json:"color,omitempty"`
-	Visible  bool   `json:"visible"`
-	// Repulsion factor to multiply the default repulsion that happens between nodes.
-	Repulsion float64 `json:"repulsion,omitempty"`
+	IsDir    bool   `json:"isDir"`
 }
 
 type Link struct {
-	From    string `json:"from"`
-	To      string `json:"to"`
-	Color   string `json:"color,omitempty"`
-	Visible bool   `json:"visible"`
-	// TODO: I don't know what Distance means really, or how does it differ from Strength
-	// Distance factor to multiply the default distance of an edge between nodes.
-	Distance float64 `json:"distance,omitempty"`
-	// Strength factor to multiply the default strength of a link.
-	Strength int `json:"strength,omitempty"`
+	From     string `json:"from"`
+	To       string `json:"to"`
+	IsDir    bool   `json:"isDir"`
+	IsCyclic bool   `json:"isCyclic"`
 }
 
 type Graph struct {
-	Nodes []Node `json:"nodes"`
-	Links []Link `json:"links"`
+	Nodes     []Node `json:"nodes"`
+	Links     []Link `json:"links"`
+	EnableGui bool   `json:"enableGui"`
 }
 
-func marshal(dt *dep_tree.DepTree[language.FileInfo], parser language.NodeParser) ([]byte, error) {
+func makeGraph(dt *dep_tree.DepTree[language.FileInfo], parser language.NodeParser) Graph {
 	out := Graph{
 		Nodes: make([]Node, 0),
 		Links: make([]Link, 0),
@@ -74,32 +64,28 @@ func marshal(dt *dep_tree.DepTree[language.FileInfo], parser language.NodeParser
 			Loc:      node.Data.Loc,
 			Size:     maxNodeSize * node.Data.Loc / maxLoc,
 			Color:    dirTree.ColorFor(dirName),
-			Visible:  true,
 		})
 
 		for _, to := range dt.Graph.FromId(node.Id) {
 			out.Links = append(out.Links, Link{
-				From:    node.Id,
-				To:      to.Id,
-				Visible: true,
+				From: node.Id,
+				To:   to.Id,
 			})
 		}
 
 		for _, parentFolder := range splitFullPaths(dirName) {
 			out.Links = append(out.Links, Link{
-				From:     node.Id,
-				To:       parentFolder,
-				Visible:  false,
-				Strength: folderLinkStrength,
+				From:  node.Id,
+				To:    parentFolder,
+				IsDir: true,
 			})
 			if _, ok := addedFolders[parentFolder]; ok {
 				continue
 			} else {
 				addedFolders[parentFolder] = true
 				out.Nodes = append(out.Nodes, Node{
-					Id:        parentFolder,
-					Visible:   false,
-					Repulsion: folderNodeRepulsion,
+					Id:    parentFolder,
+					IsDir: true,
 				})
 			}
 		}
@@ -107,12 +93,11 @@ func marshal(dt *dep_tree.DepTree[language.FileInfo], parser language.NodeParser
 
 	for el := dt.Cycles.Front(); el != nil; el = el.Next() {
 		out.Links = append(out.Links, Link{
-			From:    el.Key[0],
-			To:      el.Key[1],
-			Color:   cyclicDepColor,
-			Visible: true,
+			From:     el.Key[0],
+			To:       el.Key[1],
+			IsCyclic: true,
 		})
 	}
 
-	return json.Marshal(out)
+	return out
 }
