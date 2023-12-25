d5748d2c002f224025abe567ffff0128ac64254a
Gabriel Musat <gb.mt.me@gmail.com>
2023-12-25 20:16:26 +0100

    style: disable gosec lint

--- internal/utils/hash.go
+++ internal/utils/hash.go
@@ -1,12 +1,12 @@
 package utils
 
 import (
-	"crypto/md5"
+	"crypto/md5" //nolint:gosec
 	"fmt"
 )
 
 func hash(text string) string {
-	return fmt.Sprintf("%x", md5.Sum([]byte(text)))
+	return fmt.Sprintf("%x", md5.Sum([]byte(text))) //nolint:gosec
 }
 
 var Hash = Cached(hash)
