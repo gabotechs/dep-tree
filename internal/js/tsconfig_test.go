9cc4e28172dcb5b4019a307fb3fe2296e50aec64
Gabriel Musat <gb.mt.me@gmail.com>
2024-06-30 10:22:46 +0200

    Extend the test

--- internal/js/tsconfig_test.go
+++ internal/js/tsconfig_test.go
@@ -9,37 +9,40 @@ import (
 func TestTsConfig_ResolveFromPaths(t *testing.T) {
 	tests := []struct {
 		Name     string
-		basePath string
-		Paths    map[string][]string
+		TsConfig TsConfig
 		Import   string
 		Result   []string
 	}{
 		{
-			Name:     "without globstar",
-			basePath: "/foo/bar",
-			Paths:    map[string][]string{"@/": {"./src/"}},
-			Import:   "@/a/b/c",
-			Result:   []string{"/foo/bar/src/a/b/c"},
+			Name: "without globstar",
+			TsConfig: TsConfig{
+				path: "/foo",
+				CompilerOptions: CompilerOptions{
+					BaseUrl: "./bar/baz",
+					Paths:   map[string][]string{"@/": {"./src/"}},
+				},
+			},
+			Import: "@/a/b/c",
+			Result: []string{"/foo/bar/baz/src/a/b/c"},
 		},
 		{
-			Name:     "without globstar (2)",
-			basePath: "/foo/bar",
-			Paths:    map[string][]string{"@Environment": {"./src/environments/environments.ts"}},
-			Import:   "@Environment",
-			Result:   []string{"/foo/bar/src/environments/environments.ts"},
+			Name: "without globstar (2)",
+			TsConfig: TsConfig{
+				path: "/foo",
+				CompilerOptions: CompilerOptions{
+					BaseUrl: "./bar/baz",
+					Paths:   map[string][]string{"@Environment": {"./src/environments/environments.ts"}},
+				},
+			},
+			Import: "@Environment",
+			Result: []string{"/foo/bar/baz/src/environments/environments.ts"},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
 			a := require.New(t)
-			tsConfig := TsConfig{
-				path: tt.basePath,
-				CompilerOptions: CompilerOptions{
-					Paths: tt.Paths,
-				},
-			}
-			result := tsConfig.ResolveFromPaths(tt.Import)
+			result := tt.TsConfig.ResolveFromPaths(tt.Import)
 			a.Equal(tt.Result, result)
 		})
 	}
